5e2a375b8f7b9308775e48ad6aa8c3e6948d60cc
emosyne <eosimo@ic.ac.uk>
2023-01-19 15:11:14 +0000

    add cardiac GWAS and EPs

--- plot_results.R
+++ plot_results.R
@@ -1,21 +1,18 @@
 library(tidyverse)
 
-(results <- data.table::fread("~/GoogleDrive/WORK/CF_PhD/NF_2HH/partitioned_LDSC/2022_12_12_baseline_myGroups_100flank.results") %>% 
+(results <- data.table::fread("~/GoogleDrive/WORK/CF_PhD/NF_2HH/partitioned_LDSC/2023_01_19_NEURAL_100flank.results") %>% 
     pivot_longer(cols=!Category) %>% replace_na(list(value=1)))
-# (results <- read_excel("~/Google Drive/WORK/CF_PhD/NF_2HH/partitioned_LDSC/results 5 Nov 22.xlsx") %>% 
-#     as_tibble(.name_repair = "universal") %>% pivot_longer(cols=!Categories.) %>% 
-#     mutate(Categories. = str_replace_all(Categories., "\\s+|:", "")))
-table(results$Category)
+# table(results$Category)
 
 theme_set(theme_minimal())
 
 pdf(file = paste0("/Users/eosimo/GoogleDrive/WORK/CF_PhD/NF_2HH/partitioned_LDSC/", Sys.Date(), 
-                  "_baseline_allFANTOM_nonBRAINexp_anyFANTOMneuralEXP_PsychENCODEhighCONFforPFC_FANTOMsignESsignContactNeural_allBRAIN.pdf"), 
+                  "_NEURAL_allEnh_100flank.pdf"), 
     width = 30, height = 10, pointsize = 24)
 ggplot(aes(y=value, x = forcats::fct_inorder(Category), fill = Category), data= results)+ #[results$name=="Enrichment",]
   facet_grid(cols = vars(name), scales="free")+
   geom_bar(stat = "identity")+
-  geom_text(aes(label = round(value,3), x = forcats::fct_inorder(Category), y = value), position = position_dodge(width = 0.8), hjust = -0.5, size=2)+
+  geom_text(aes(label = round(value,2), x = forcats::fct_inorder(Category), y = value), position = position_dodge(width = 0.2), hjust = -0.5, size=2)+
   # scale_fill_brewer(palette = "Set1") +
   coord_flip()+ theme(legend.position = "none")
 dev.off()
